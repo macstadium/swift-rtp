7d1318d66364ce123e3efc2a7627cbe9169add76
Cameron Roe <croe@macstadium.com>
2023-04-06 11:19:10 -0500

    Reformat to 4 space indents

--- Tests/RTPTests/ConnectionTests.swift
+++ Tests/RTPTests/ConnectionTests.swift
@@ -1,24 +1,24 @@
 #if canImport(Network)
 
-	import Network
-	import XCTest
+    import Network
+    import XCTest
 
-	@testable import RTP
+    @testable import RTP
 
-	class RTPConnectionTests: XCTestCase {
-		func testConnectionStop() {
-			let conn = RTP.Connection(host: "localhost", port: 12345) { _ in }
-			XCTAssertEqual(conn.conn.state, .setup)
-			conn.start()
-			sleep(1)
-			XCTAssertEqual(conn.conn.state, .ready)
-			conn.stop()
-			sleep(1)
-			XCTAssertEqual(conn.conn.state, .cancelled)
-			conn.conn.restart()
-			sleep(1)
-			XCTAssertEqual(conn.conn.state, .cancelled) // NWConnection instances cannot be restarted once cancelled
-		}
-	}
+    class RTPConnectionTests: XCTestCase {
+        func testConnectionStop() {
+            let conn = RTP.Connection(host: "localhost", port: 12345) { _ in }
+            XCTAssertEqual(conn.conn.state, .setup)
+            conn.start()
+            sleep(1)
+            XCTAssertEqual(conn.conn.state, .ready)
+            conn.stop()
+            sleep(1)
+            XCTAssertEqual(conn.conn.state, .cancelled)
+            conn.conn.restart()
+            sleep(1)
+            XCTAssertEqual(conn.conn.state, .cancelled) // NWConnection instances cannot be restarted once cancelled
+        }
+    }
 
 #endif
--- Tests/RTPTests/PacketizerTests.swift
+++ Tests/RTPTests/PacketizerTests.swift
@@ -3,15 +3,15 @@ import XCTest
 @testable import RTP
 
 class PacketizerTests: XCTestCase {
-	func testIncrementOverflow() {
-		var packetizer = RTP.Packetizer(for: .opus, sequenceNumber: .max, timestamp: .max)
+    func testIncrementOverflow() {
+        var packetizer = RTP.Packetizer(for: .opus, sequenceNumber: .max, timestamp: .max)
 
-		packetizer.increment(48000)
-		XCTAssertEqual(packetizer.sequenceNumber, 0)
-		XCTAssertEqual(packetizer.timestamp, 48000 - 1)
+        packetizer.increment(48000)
+        XCTAssertEqual(packetizer.sequenceNumber, 0)
+        XCTAssertEqual(packetizer.timestamp, 48000 - 1)
 
-		packetizer.increment(48000)
-		XCTAssertEqual(packetizer.sequenceNumber, 1)
-		XCTAssertEqual(packetizer.timestamp, 48000 * 2 - 1)
-	}
+        packetizer.increment(48000)
+        XCTAssertEqual(packetizer.sequenceNumber, 1)
+        XCTAssertEqual(packetizer.timestamp, 48000 * 2 - 1)
+    }
 }
