ce29339365a1c3afbc4d9db5b54008823070f6db
Cameron Roe <croe@macstadium.com>
2023-04-06 17:07:46 -0500

    Add test for packetizing Opus

--- Tests/RTPTests/PacketizerTests.swift
+++ Tests/RTPTests/PacketizerTests.swift
@@ -14,4 +14,20 @@ class PacketizerTests: XCTestCase {
         XCTAssertEqual(packetizer.sequenceNumber, 1)
         XCTAssertEqual(packetizer.timestamp, 48000 * 2 - 1)
     }
+
+    func testOpusPacketize() throws {
+        var packetizer = RTP.Packetizer(for: .opus, sequenceNumber: 0, timestamp: 0)
+
+        let fakePayload = Data(count: 1000)
+        
+        // Make sure first packet does not throw
+        XCTAssertNoThrow(try packetizer.packetize(fakePayload, 48000))
+
+        // Capture second packet to assert against
+        let packet = try packetizer.packetize(fakePayload, 48000)
+        
+        XCTAssertEqual(packet.payloadType, PayloadType.opus)
+        XCTAssertEqual(packet.sequenceNumber, 1)
+        XCTAssertEqual(packet.timestamp, 48000)
+    }
 }
